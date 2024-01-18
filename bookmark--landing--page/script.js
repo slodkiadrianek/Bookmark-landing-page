be1f8ff43bb57814ec6d7dc6ca18ab2e5e0039c9
Adrian Kurek <136102234+slodkiadrianek@users.noreply.github.com>
2024-01-18 21:43:12 +0100

    Add files via upload

--- bookmark--landing--page/script.js
+++ bookmark--landing--page/script.js
@@ -5,6 +5,8 @@ const section2BtnCon = document.querySelector('.section__2__btn__con')
 const section2Img = document.querySelector('.section__2__img')
 const section2Right = document.querySelectorAll('.section__2__right')
 const section2BoxCon = document.querySelectorAll('.section__2__box__con')
+const section4BoxCon = document.querySelectorAll('.section__4__box__con')
+const boxP = document.querySelector('.box__p')
 
 section2BtnCon.addEventListener('click', function(e){
     const target = e.target
@@ -19,7 +21,6 @@ section2BtnCon.addEventListener('click', function(e){
             section2Right.forEach(el => el.dataset.text === '1' ? el.classList.add('section__2__right__active') : console.log(`yes`))
             section2BoxCon.forEach(el => el.dataset.img === '1' ? el.classList.add('section__2__box__con__active') : console.log(`yes`))
         }else if (dataSet === '2'){
-
             section2Right.forEach(el => el.dataset.text === '2' ? el.classList.add('section__2__right__active') : console.log(`yes`))
             section2BoxCon.forEach(el => el.dataset.img === '2' ? el.classList.add('section__2__box__con__active') : console.log(`yes`))
         }else{
@@ -29,5 +30,15 @@ section2BtnCon.addEventListener('click', function(e){
     }
 })
 
+section4BoxCon.forEach(el => {
+    el.addEventListener('click', function(e){
+        const target = e.target.closest('.section__4__box')
+        target.children[1].classList.toggle('box__p__active')
+       const arrowTurn = e.target.children[1];
+       arrowTurn.classList.toggle('arrow__active')
+      
+       
+    })
+})
 
     
